52e9d2c7283b00ad73383f1a2bd11f1268f238d4
wuwangang <2938567281@qq.com>
2021-09-07 17:14:06 +0800

    android demo

---
+++ app/src/main/java/com/ldcc/test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ldcc.test;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        TextView tv_one = findViewById(R.id.tv_one);
+//        tv_one.setText("雪碧");
+    }
+}
