c40a942f308d76dc7c6088bc4f479f89190b76d2
wuwangang <2938567281@qq.com>
2021-09-10 16:52:26 +0800

    通知功能

--- mynotification/src/main/java/com/ldcc/mynotification/MainActivity.java
+++ mynotification/src/main/java/com/ldcc/mynotification/MainActivity.java
@@ -1,19 +1,53 @@
 package com.ldcc.mynotification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NotificationManager manager;
+
+    private Notification notification;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("test", "测试通知",
+                    NotificationManager.IMPORTANCE_HIGH);
+            manager.createNotificationChannel(channel);
+        }
+        notification = new NotificationCompat.Builder(this, "test")
+                .setContentTitle("官方通知")
+                .setContentText("世界那么大，想去走走吗")
+                .setSmallIcon(R.drawable.ic_baseline_account_box_24)
+                .build();
+
+    }
+
+    private Bitmap drawableToBitamp(Drawable drawable)
+    {
+        BitmapDrawable bd = (BitmapDrawable) drawable;
+        return bd.getBitmap();
     }
 
     public void sendNotification(View view) {
+        manager.notify(1, notification);
+    }
 
+    public void cancelNotification(View view) {
     }
 }
